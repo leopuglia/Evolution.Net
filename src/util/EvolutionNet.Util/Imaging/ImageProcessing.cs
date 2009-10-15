73f2bd66960dd39079f5a8fef8587ab3ba774da9
leopuglia <leopuglia@48eeed25-3eef-48b0-9f8d-79fca588c099>
2009-10-15 21:40:46 +0000

    Improved the Image Processing lib.
    git-svn-id: https://evolution-net.svn.sourceforge.net/svnroot/evolution-net/trunk@44 48eeed25-3eef-48b0-9f8d-79fca588c099

--- src/util/EvolutionNet.Util/Imaging/ImageProcessing.cs
+++ src/util/EvolutionNet.Util/Imaging/ImageProcessing.cs
@@ -2,22 +2,164 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
-using AForge.Imaging.Filters;
+using log4net;
 
 namespace EvolutionNet.Util.Imaging
 {
 	public static class ImageProcessing
 	{
-		public static void ResizeOnStream(string imgPath, Size newSize, Stream stream)
+		private static readonly ILog log = LogManager.GetLogger(typeof(ImageProcessing));
+
+		private const int QUALITY = 75;
+
+		#region Jpeg
+
+		public static void SaveJpegOnStream(Stream stream, Bitmap img, long quality)
 		{
-			Bitmap imgResized = Resize(imgPath, newSize);
-			imgResized.Save(stream, ImageFormat.Jpeg);
+			// Encoder parameter for image quality
+			EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, quality);
+
+			// Jpeg image codec
+			ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+			if (jpegCodec == null)
+				return;
+
+			EncoderParameters encoderParams = new EncoderParameters(1);
+			encoderParams.Param[0] = qualityParam;
+
+			img.Save(stream, jpegCodec, encoderParams);
+		}
+
+		public static void SaveJpeg(string path, Bitmap img, long quality)
+		{
+			// Encoder parameter for image quality
+			EncoderParameter qualityParam = new EncoderParameter(Encoder.Quality, quality);
+
+			// Jpeg image codec
+			ImageCodecInfo jpegCodec = GetEncoderInfo(ImageFormat.Jpeg);
+
+			if (jpegCodec == null)
+				return;
+
+			EncoderParameters encoderParams = new EncoderParameters(1);
+			encoderParams.Param[0] = qualityParam;
+
+			img.Save(path, jpegCodec, encoderParams);
+		}
+
+		public static ImageCodecInfo GetEncoderInfo(ImageFormat format)
+		{
+			ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+
+			foreach (ImageCodecInfo codec in codecs)
+			{
+				if (codec.FormatID == format.Guid)
+					return codec;
+			}
+			return null;
+		}
+
+		public static ImageCodecInfo GetEncoderInfo(string mimeType)
+		{
+			// Get image codecs for all image formats
+			ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+			// Find the correct image codec
+			foreach (ImageCodecInfo codec in codecs)
+				if (codec.MimeType == mimeType)
+					return codec;
+			return null;
+		}
+
+		#endregion
+
+		#region Crop
+
+		public static void CropOnStream(string fileName, Rectangle cropArea, Stream stream)
+		{
+			Bitmap imgCrop = Crop(fileName, cropArea);
+			SaveJpegOnStream(stream, imgCrop, QUALITY);
+			imgCrop.Dispose();
+		}
+
+		public static Bitmap Crop(string fileName, Rectangle cropArea)
+		{
+			Bitmap img = new Bitmap(fileName);
+			Bitmap imgCrop = Crop(img, cropArea);
+			img.Dispose();
+			return imgCrop;
+		}
+
+		public static Bitmap Crop(Bitmap img, Rectangle cropArea)
+		{
+			Bitmap imgCrop = img.Clone(cropArea, img.PixelFormat);
+			img.Dispose();
+			return imgCrop;
+		}
+
+		#endregion
+
+		#region Resize
+
+		public static void ResizeOnStream(Stream streamRead, Size newSize, Stream streamWrite)
+		{
+			Bitmap img = new Bitmap(streamRead);
+			Bitmap imgResized = Resize(img, newSize);
+			img.Dispose();
+			imgResized.Save(streamWrite, ImageFormat.Jpeg);
 			imgResized.Dispose();
+		}
+
+		public static void ResizeOnStream(string fileName, Size newSize, Stream streamWrite)
+		{
+			Bitmap imgResized = Resize(fileName, newSize);
+			imgResized.Save(streamWrite, ImageFormat.Jpeg);
+			imgResized.Dispose();
+		}
+
+		public static void ResizeOnStream(Bitmap img, Size newSize, Stream streamWrite)
+		{
+			Bitmap imgResized = Resize(img, newSize);
+			imgResized.Save(streamWrite, ImageFormat.Jpeg);
+			imgResized.Dispose();
+		}
+
+		public static void ResizeAndSaveJpeg(Stream streamRead, string newFileName, Size newSize)
+		{
+			Bitmap img = new Bitmap(streamRead);
+			Bitmap imgResized = Resize(img, newSize);
+			img.Dispose();
+			SaveJpeg(newFileName, imgResized, QUALITY);
+			imgResized.Dispose();
+		}
+
+		public static void ResizeAndSaveJpeg(string fileName, string newFileName, Size newSize)
+		{
+			Bitmap imgResized = Resize(fileName, newSize);
+			SaveJpeg(newFileName, imgResized, QUALITY);
+			imgResized.Dispose();
+		}
+
+		public static void ResizeAndSaveJpeg(Bitmap img, string newFileName, Size newSize)
+		{
+			Bitmap imgResized = Resize(img, newSize);
+			img.Dispose();
+			SaveJpeg(newFileName, imgResized, QUALITY);
+			imgResized.Dispose();
+		}
+
+		public static Bitmap Resize(Stream streamRead, Size newSize)
+		{
+			Bitmap img = new Bitmap(streamRead);
+			Bitmap imgResized = Resize(img, newSize);
+			img.Dispose();
+			return imgResized;
 		}
 
 		public static Bitmap Resize(string fileName, Size newSize)
 		{
-			Bitmap img = (Bitmap)Image.FromFile(fileName);
+			Bitmap img = new Bitmap(fileName);
 			Bitmap imgResized = Resize(img, newSize);
 			img.Dispose();
 			return imgResized;
@@ -37,24 +179,36 @@ namespace EvolutionNet.Util.Imaging
 			int destWidth = (int)(imgWidth * nPercent);
 			int destHeight = (int)(imgHeight * nPercent);
 
-			//			AForge.Imaging.Image.FormatImage(ref img);
-			ResizeBicubic resizeBicubic = new ResizeBicubic(destWidth, destHeight);
-			Bitmap imgResized = resizeBicubic.Apply(img);
+//			ResizeBicubic resizeBicubic = new ResizeBicubic(destWidth, destHeight);
+//			Bitmap imgResized = resizeBicubic.Apply(img);
+
+			Bitmap imgResized = GetNewBitmap(img, destWidth, destHeight);
+
+			Graphics g = Graphics.FromImage(imgResized);
+			g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+			g.DrawImage(img, 0, 0, destWidth, destHeight);
+
+			g.Dispose();
 			img.Dispose();
 
 			return imgResized;
 		}
 
-		public static void ResizeFixedSizeOnStream(string imgPath, Size newSize, Color paddingColor, Stream stream)
+		#endregion
+
+		#region ResizeFixedSize
+
+		public static void ResizeFixedSizeOnStream(string fileName, Size newSize, Color paddingColor, Stream stream)
 		{
-			Bitmap imgResized = ResizeFixedSize(imgPath, newSize, paddingColor);
+			Bitmap imgResized = ResizeFixedSize(fileName, newSize, paddingColor);
 			imgResized.Save(stream, ImageFormat.Jpeg);
 			imgResized.Dispose();
 		}
 
 		public static Bitmap ResizeFixedSize(string fileName, Size newSize, Color paddingColor)
 		{
-			Bitmap img = (Bitmap)Image.FromFile(fileName);
+			Bitmap img = new Bitmap(fileName);
 			Bitmap imgResized = ResizeFixedSize(img, newSize, paddingColor);
 			img.Dispose();
 			return imgResized;
@@ -87,10 +241,7 @@ namespace EvolutionNet.Util.Imaging
 				destY = (int)((newSize.Height - (img.Height * nPercent)) / 2);
 			}
 
-			Bitmap imgResized = new Bitmap(newSizeWidth, newSizeHeight,
-			                            PixelFormat.Format24bppRgb);
-			imgResized.SetResolution(img.HorizontalResolution,
-			                         img.VerticalResolution);
+			Bitmap imgResized = GetNewBitmap(img, newSizeWidth, newSizeHeight);
 
 			Graphics grPhoto = Graphics.FromImage(imgResized);
 			grPhoto.Clear(paddingColor);
@@ -108,33 +259,22 @@ namespace EvolutionNet.Util.Imaging
 			return imgResized;
 		}
 
-		public static string IntToHex(int number)
+		#endregion
+
+		private static Bitmap GetNewBitmap(Image img, int destWidth, int destHeight)
 		{
-			return string.Format("{0:x}", number);
-		}
+			log.DebugFormat("Creating a new bitmap with {0} width and {1} height", destWidth, destHeight);
 
-		public static int HexToInt(string hexString)
-		{
-			return int.Parse(hexString,
-			                 System.Globalization.NumberStyles.HexNumber, null);
-		}
-
-		public static Color ColorFromHexadecimal(string hexa)
-		{
-			int red = HexToInt(hexa.Substring(1, 2));
-			int green = HexToInt(hexa.Substring(3, 2));
-			int blue = HexToInt(hexa.Substring(5, 2));
-
-			return Color.FromArgb(red, green, blue);
-		}
-
-		public static int ArgbFromHexadecimal(string hexa)
-		{
-			int red = HexToInt(hexa.Substring(1, 2));
-			int green = HexToInt(hexa.Substring(3, 2));
-			int blue = HexToInt(hexa.Substring(5, 2));
-
-			return Color.FromArgb(red, green, blue).ToArgb();
+			Bitmap imgResized = new Bitmap(destWidth, destHeight,
+										   PixelFormat.Format24bppRgb);
+			foreach (ExifTag exifTag in new ExifTagCollection(img))
+			{
+				log.DebugFormat("Adding propertyitem {0}: {1}", exifTag.Description, exifTag.Value);
+				imgResized.SetPropertyItem(img.GetPropertyItem(exifTag.Id));
+			}
+			imgResized.SetResolution(img.HorizontalResolution,
+									 img.VerticalResolution);
+			return imgResized;
 		}
 
 	}
