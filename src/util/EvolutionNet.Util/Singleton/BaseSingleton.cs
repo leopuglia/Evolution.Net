12a06245fc7a69a483ff52a7fcd6a2048a06ab0b
leopuglia <leopuglia@48eeed25-3eef-48b0-9f8d-79fca588c099>
2010-12-14 16:59:09 +0000

    Created a base singleton class to define singleton classes
    git-svn-id: https://evolution-net.svn.sourceforge.net/svnroot/evolution-net/trunk@103 48eeed25-3eef-48b0-9f8d-79fca588c099

---
+++ src/util/EvolutionNet.Util/Singleton/BaseSingleton.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace EvolutionNet.Util.Singleton
+{
+    public abstract class BaseSingleton<T>
+    {
+		#region Thread Safe Singleton
+
+        public static T Instance
+		{
+			get { return Nested.instance; }
+		}
+
+        public static T CreateInstance()
+        {
+            // TODO: Teoricamente um singleton só deve ter uma instância, mas há casos em que outras instâncias podem ser necessárias
+            return Nested.CreateLocalInstance();
+        }
+
+        private class Nested
+		{
+			// Explicit static constructor to tell C# compiler
+			// not to mark type as beforefieldinit
+			static Nested()
+			{
+			}
+
+            internal static readonly T instance = CreateLocalInstance();
+
+            internal static T CreateLocalInstance()
+            {
+                return Activator.CreateInstance<T>();
+            }
+		}
+
+		#endregion
+
+    }
+}
